54e44d167e796b5f7341de72cf00181e74c18f5b
Simon Law <simondw.law@outlook.com>
2019-06-18 14:49:58 +0000

    adding API keys for stripe app

--- env.py
+++ env.py
@@ -2,3 +2,6 @@ import os
 
 os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIASW3IKR3773ZUFVI6")
 os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_eYZ3TMvEMkHBSfmF1hJhkSEb00CGz5GJgG")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_iIl1ikuDlbtFnaJxFpIzvMuO00WsxvmCT4")
